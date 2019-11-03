b7141f47feb4ce4d0880e70e5807e0abfd7dc584
Darian Lio <darianlio97@gmail.com>
2019-11-03 00:46:41 -0400

    added some scroll animation

--- web/src/components/header/nav.js
+++ web/src/components/header/nav.js
@@ -2,49 +2,69 @@ import React from 'react'
 import { Link } from "gatsby"
 import "../../styles/header/nav.scss"
 
-const Nav = () => (
-    <div id="navbar">
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">ABOUT</Link>
-                </li>
-                <li>
-                    <Link to="/">EXPERIENCE</Link>
-                </li>
-                <li>
-                    <Link to="/">EDUCATION</Link>
-                </li>
-                <li>
-                    <Link to="/">PORTFOLIO</Link>
-                </li>
-                <li>
-                    <Link to="/">CONTACT</Link>
-                </li>
-                <li>
-                    <Link to="/">RESUME</Link>
-                </li>
-            </ul>
-        </nav>
-        <Link to="/">
-            <div id="logo">  
-                <span>L</span>
-                <span class="logo-left">D</span>
-                <br/>
-                <span class="logo-I">I</span>
-                <span class="logo-left-left">A</span>
-                <br/>
-                <span>O</span>
-                <span class="logo-left">R</span>
-                <br/>
-                <span class="logo-dar">I</span>
-                <br/>
-                <span class="logo-dar-2">A</span>
-                <br/>
-                <span class="logo-dar-2">N</span>
+class Nav extends React.Component {
+    state = {
+        color: 'white'
+    }
+
+    listenScrollEvent = e => {
+        if (window.scrollY > 400) {
+            this.setState({color: 'black'})
+        } else {
+            this.setState({color: 'white'})
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.listenScrollEvent)
+    }
+
+    render() {
+        return (
+            <div id="navbar" style={{color: this.state.color}}>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">ABOUT</Link>
+                        </li>
+                        <li>
+                            <Link to="/">EXPERIENCE</Link>
+                        </li>
+                        <li>
+                            <Link to="/">EDUCATION</Link>
+                        </li>
+                        <li>
+                            <Link to="/">PORTFOLIO</Link>
+                        </li>
+                        <li>
+                            <Link to="/">CONTACT</Link>
+                        </li>
+                        <li>
+                            <Link to="/">RESUME</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Link to="/">
+                    <div id="logo">  
+                        <span>L</span>
+                        <span class="logo-left">D</span>
+                        <br/>
+                        <span class="logo-I">I</span>
+                        <span class="logo-left-left">A</span>
+                        <br/>
+                        <span>O</span>
+                        <span class="logo-left">R</span>
+                        <br/>
+                        <span class="logo-dar">I</span>
+                        <br/>
+                        <span class="logo-dar-2">A</span>
+                        <br/>
+                        <span class="logo-dar-2">N</span>
+                    </div>
+                </Link>
             </div>
-        </Link>
-    </div>
-)
+        )
+    }
+}
 
 export default Nav
--- web/src/components/socialmedia/socialmedia.js
+++ web/src/components/socialmedia/socialmedia.js
@@ -1,16 +1,35 @@
 import React from 'react'
 import "../../styles/social-media/socialmedia.scss"
 
-const SocialMedia = () => (
-    <div class="social-media">
-        <ul class="social-media-list">
-            <li class="social-list"><a href="https://github.com/darianlio/" target="__blank" rel="noopener noreferrer" aria-label="github"><i class="fab fa-github"></i></a></li>
-            <li class="social-list"><a href="https://www.linkedin.com/in/darianlio/" target="__blank" rel="noopener noreferrer" aria-label="linkedin"><i class="fab fa-linkedin-in"></i></a></li>
-            <li class="social-list"><a href="https://www.instagram.com/notdurian/" target="__blank" rel="noopener noreferrer" aria-label="instagram"><i class="fab fa-instagram"></i></a></li>
-            <li class="social-list"><a href="mailto:darianlio97@gmail.com" rel="nofollow noopener noreferrer" aria-label="email"><i class="fas fa-paper-plane"></i></a></li>
-            <li class="social-list social-list-after"><a href="https://www.google.com/maps/place/Toronto,+ON/@43.6565353,-79.6010302,10z/data=!3m1!4b1!4m5!3m4!1s0x89d4cb90d7c63ba5:0x323555502ab4c477!8m2!3d43.653226!4d-79.3831843" target="__blank" rel="nofollow noopener noreferrer" aria-label="location"><i className="fas fa-map-marker-alt" /></a></li>
-        </ul>
-    </div>
-)
+class SocialMedia extends React.Component {
+    state = {
+        color: 'white'
+    }
 
+    listenScrollEvent = e => {
+        if (window.scrollY > 400) {
+            this.setState({color: 'black'})
+        } else {
+            this.setState({color: 'white'})
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.listenScrollEvent)
+    }
+
+    render() {
+        return (
+            <div class="social-media" style={{color: this.state.color}}>
+                <ul class="social-media-list">
+                    <li class="social-list"><a href="https://github.com/darianlio/" target="__blank" rel="noopener noreferrer" aria-label="github"><i class="fab fa-github"></i></a></li>
+                    <li class="social-list"><a href="https://www.linkedin.com/in/darianlio/" target="__blank" rel="noopener noreferrer" aria-label="linkedin"><i class="fab fa-linkedin-in"></i></a></li>
+                    <li class="social-list"><a href="https://www.instagram.com/notdurian/" target="__blank" rel="noopener noreferrer" aria-label="instagram"><i class="fab fa-instagram"></i></a></li>
+                    <li class="social-list"><a href="mailto:darianlio97@gmail.com" rel="nofollow noopener noreferrer" aria-label="email"><i class="fas fa-paper-plane"></i></a></li>
+                    <li class="social-list social-list-after"><a href="https://www.google.com/maps/place/Toronto,+ON/@43.6565353,-79.6010302,10z/data=!3m1!4b1!4m5!3m4!1s0x89d4cb90d7c63ba5:0x323555502ab4c477!8m2!3d43.653226!4d-79.3831843" target="__blank" rel="nofollow noopener noreferrer" aria-label="location"><i className="fas fa-map-marker-alt" /></a></li>
+                </ul>
+            </div>
+        )
+    }
+}
 export default SocialMedia
